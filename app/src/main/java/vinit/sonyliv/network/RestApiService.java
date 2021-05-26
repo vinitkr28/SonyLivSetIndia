0f87eb29fc3bfb06116bd271ac7a16404636b193
Vinit <vinit.kumar_1@signify.com>
2021-05-26 10:27:12 +0530

    Header in RestApiService.java commented

--- app/src/main/java/vinit/sonyliv/network/RestApiService.java
+++ app/src/main/java/vinit/sonyliv/network/RestApiService.java
@@ -9,14 +9,18 @@ import vinit.sonyliv.model.ProductWrapper;
 public interface RestApiService {
 
     // For the time being Fk-Affiliate-Id, Fk-Affiliate-Token har corded here
-    @Headers({
+    /*@Headers({
             "Accept: application/json",
-            "Fk-Affiliate-Id: trushstud",
-            "Fk-Affiliate-Token: 330b1fc28fbd40bb94b9ae397dfb172d"
-    })
+            "Fk-Affiliate-Id: abcdefg",
+            "Fk-Affiliate-Token: 1234567890"
+    })*/
 //    @GET("search.json?query=sony+mobiles&resultCount=10")
     @GET("search.json?")
     Call<ProductWrapper> getProducts(@Query("query") String searchKey);
+
+
+
+
 
 //      "https://next.json-generator.com/api/json/get/EJGbhABKc/"
 //      @GET("cpQFgtnOYy?indent=2")
